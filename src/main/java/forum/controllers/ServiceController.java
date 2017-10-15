254b4db42267fc835353ab0f2cf5245384199721
MikeGus <vfc17@yandex.ru>
2017-10-15 21:57:44 +0300

    fixed status controllers

--- src/main/java/forum/controllers/ServiceController.java
+++ src/main/java/forum/controllers/ServiceController.java
@@ -1,5 +1,9 @@
 package forum.controllers;
 
+import forum.Jdbc.JdbcForum;
+import forum.Jdbc.JdbcPost;
+import forum.Jdbc.JdbcThread;
+import forum.Jdbc.JdbcUser;
 import forum.models.StatusModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,13 +20,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/service")
 public class ServiceController {
 
+    private JdbcUser jdbcUser;
+    private JdbcForum jdbcForum;
+    private JdbcPost jdbcPost;
+    private JdbcThread jdbcThread;
+
     @RequestMapping(value = "clear", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> clearDB() {
+        jdbcUser.clear();
+        jdbcForum.clear();
+        jdbcPost.clear();
+        jdbcThread.clear();
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
     @RequestMapping(value = "status", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StatusModel> getStatusDB() {
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+
+        Integer userNumber = jdbcUser.status();
+        Integer forumNumber = jdbcForum.status();
+        Integer postNumber = jdbcPost.status();
+        Integer threadNumber = jdbcThread.status();
+
+        return ResponseEntity.status(HttpStatus.OK).body(new StatusModel(forumNumber, postNumber,
+                threadNumber, userNumber));
     }
 }
